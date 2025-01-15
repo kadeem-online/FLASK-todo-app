0a178b7bd7b9652580d1c49d1b877dc749b0b0be
kadeem-online <kadeemalexander254@gmail.com>
2025-01-15 16:56:18 +0300

    UPDATE: implemented todo creation logic
    - Todo items can now be added from the frontend and basic toasts are returned showing the success of the actions.

--- app/blueprints/todo.py
+++ app/blueprints/todo.py
@@ -1,7 +1,49 @@
-from flask import ( Blueprint, render_template )
+from app.db import ( get_engine, TodoModel )
+from flask import ( Blueprint, flash, render_template )
+from flask_wtf import ( FlaskForm )
+from sqlalchemy.orm import ( Session )
+from wtforms import ( StringField, SubmitField )
+from wtforms.validators import ( DataRequired, Length )
 
 todo_blueprint = Blueprint('main', __name__ )
 
-@todo_blueprint.get("/")
+class NewTodoForm(FlaskForm):
+    description = StringField(
+        'Todo Description',
+        validators=[
+            DataRequired(message="The todo item cannot be empty."),
+            Length(min=4, message="The todo item must be at least 3 characters long."),
+            # Regexp(r'^\S.*\S$|^\S$', message="The todo item cannot start or end with whitespace."),
+        ]
+    )
+    submit = SubmitField('Add Todo')
+
+@todo_blueprint.route("/", methods=['GET', 'POST'])
 def index():
-  return render_template("index.html")
+  new_todo_form = NewTodoForm()
+  
+  if new_todo_form.validate_on_submit():
+    # get the contents of the todo
+    _todo_description = new_todo_form.description.data
+    
+    # create new Todo item
+    _todo = TodoModel(description=_todo_description)
+    
+    # save the todo
+    _engine = get_engine()
+    with Session(_engine) as session:
+      session.add(_todo)
+      session.commit()
+      
+      # clear description field on successful entry
+      new_todo_form.description.data = ""
+      
+      # flash success message
+      flash("Task Added", "todo-added")
+  elif new_todo_form.errors:
+    # flash all form validation errors for the fields
+    for field, errors in new_todo_form.errors.items():
+      for error in errors:
+        flash(f"{error}", "error")
+
+  return render_template("index.html", form=new_todo_form )
